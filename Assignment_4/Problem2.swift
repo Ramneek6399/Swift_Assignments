af7de406304b831fc22916664631abf92131be36
Ramneek6399 <106309938+Ramneek6399@users.noreply.github.com>
2022-09-02 17:46:23 -0700

    completed

--- Assignment_4/Problem2.swift
+++ Assignment_4/Problem2.swift
@@ -7,22 +7,75 @@
 //}
 //func0 = fakeFuntion0;
 
-var func1 : (Int, Int) -> Int {
+func ffunc1(a :Int, b: Int) -> Int {
+    return (a - b)
 }
 
-var func2 : (String)
+func ffunc2(a :String) {
+    print(a)
+}
 
-var func3 : () -> Int
+func ffunc3() -> Int{
+    return 3
+}
 
-var func4 : () ->[()->Void]
+func ffunc4() ->[()->Void] {
+    return[{print("Print")}]
+}
 
-var func5 : ([(Int)->Void]) -> [String]
+func ffunc5([(Int)->Void]) -> [String]{
+    return ["A","b","c"]
+}
 
-var func6 : ([(Int)->Void]) -> [(Int)->void]
+func ffunc6([(Int)->Void]) -> [(Int)->void]{
+    return a
+}
 
-var func7 : ([(Int)->Void]?) -> [(Int)->void]?
+func ffunc7([(Int)->Void]?) -> [(Int)->void]?{
+    return b
+}
 
-var func8 : ([(Int)->Void]?) -> [(Int)->void]?
+func ffunc8([(Int)->Void]?) -> [(Int)->void]?{
+    return a
+}
 
-var func9 : () -> ((Int)->Int, Int)?
+func ffunc9() -> ((Int)->Int, Int)?{
+    return ({a in a}, 1)
+}
+
+func main(){
+    let func1 : (Int, Int) -> Int
+    func1 = ffunc1
+    
+    let func2 : (String) -> Void
+    func2 = ffunc2
+    
+    let func3 : () -> Int
+    func2 = ffunc3
+    
+    let func4 : () -> [() -> Void]
+    func4 = ffunc4
+    
+    let func5 : ([(Int) -> Void]) -> [String])
+    func5 = ffunc5
+    
+    
+    let func6 : ([(Int) -> Void]) -> [(Int) -> Void]
+    func6 = ffunc6
+    
+    let func7 : ([])
+    func7 = ffunc7
+    
+    let func8 : ()
+    func8 = ffunc8
+    
+    let func9 :
+    func9 = ffunc9
+    
+    
+    
+    print(func1)
+    print(
+}
+
 
--- Assignment_4/Problem5.swift
+++ Assignment_4/Problem5.swift
@@ -11,6 +11,53 @@
 //Then  implement  the  filter  function  mentioned  above.  The  filter  function  returns  the  list  of  all
 //numbers from the input list on which all predicates return true.
 
-func filter ( array: [Int], Predicate: [(Int)-> Bool]) -> [Int]? {
+func filter ( list : [Int], Predicate: [(Int)-> Bool]) -> [Int]? {
+    var array : [Int] = []
+    
+    for num in list {
+        var isalltrue = true
+        for predicate in Predicate{
+            if !predicate(num){
+                isalltrue = false
+            }
+        }
+        if isalltrue == true{
+            array.append(num)
+        }
+        
+    }
+    return array
     
 }
+func isPrime (num: Int) -> Bool {
+    
+    if num < 2 {
+        return false
+    }
+    for a in 2..<num{
+        if num % a == 0 {
+           return false
+        }
+        
+    }
+    return true
+}
+func main(){
+    let list = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+    
+    let Predicate1: (Int) -> Bool = { a in a % 2 != 0 }
+    let Predicate2: (Int) -> Bool = isPrime
+    let Predicate3: (Int) -> Bool = {a in a % 2 != 0 && isPrime(num: a)}
+    let Predicate4: (Int) -> Bool = {a in a % 7 != 0}
+    
+    let predicates = [Predicate1, Predicate2, Predicate3, Predicate4]
+    
+    guard let result = filter(list: list, Predicate: predicates) else {
+        return
+    }
+    print(result)
+    
+    
+    
+}
+main()
